6ed7c7565d7c9173ad173bcad34a2091f7d54d53
Samira Olivieri <art0066441@aluno.firjansenaisesi.com.br>
2026-03-26 13:33:25 -0300

    Terceiro Commit

--- config/db.js
+++ config/db.js
@@ -2,20 +2,18 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: '127.0.0.1', //
+    host: '127.0.0.1',
     user: 'root',
     password: 'alunolab',
     database: 'sistema_dev',
-    port: 3303, //
+    port: 3303,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    // Garante compatibilidade com versões novas do MySQL
     enableKeepAlive: true,
     keepAliveInitialDelay: 0
 });
 
-// Teste de conexão imediato
 pool.getConnection()
     .then(() => console.log("✅ Conectado ao MySQL com sucesso!"))
     .catch(err => {
